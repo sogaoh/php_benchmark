ac457028a084b2ff11c6d58d521a4ff03402ae7a
flum1025 <flum.1025@gmail.com>
2015-09-21 16:00:05 +0900

    高速化した

--- php_benchmark.php
+++ php_benchmark.php
@@ -6,21 +6,20 @@ error_reporting(E_ALL & ~E_NOTICE);
 $times = 100000000;
 echo "${times}回ループします\n";
 
+$str = array_merge ( range ( 'a', 'z' ), range ( '0', '9' ), range ( 'A', 'Z' ) );
+
 $starttime = microtime(TRUE);
 $startmsec = explode('.', $starttime);
 echo date("i:s",$starttime) . "." . $startmsec[1] . "\n";
 
 $count = 0;
-while ($count < $times){
-    $str = array_merge(range('a', 'z'), range('0', '9'), range('A', 'Z'));
-    $r_str = null;
-    for ($i = 0; $i < 10; $i++) {
-        $r_str .= $str[rand(0, count($str))];
-    }
-		$count++;
+while ( $count < $times ) {
+	$r_str = null;
+	for($i = 0; $i < 10; $i ++) {
+		$r_str .= $str [rand ( 0, count ( $str ) )];
+	}
+	$count ++;
 }
-
-
 
 $endtime = microtime(TRUE);
 $endmsec = explode('.', $endtime);
