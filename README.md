e93a39903d402beb27cbae5398f2241b90611b31
sogaoh <sogaoh@ant-in-giant.work>
2020-12-25 11:29:00 +0900

    chore: adjust(s)
    - 配列インデックスが領域を超えないように調整
    - 適度な時間で終わるようにループ回数を減らした
    - mb_language 等のコメントアウトはしなくて良かったかも

--- README.md
+++ README.md
@@ -1,31 +1,38 @@
 ﻿php_benchmark
 ===========
 
-##What is it?
 
-phpで作られた簡易ベンチマーク。
+## What is it?
+
+phpで作られた簡易ベンチマーク、のようです。[^1]  
 大文字小文字のアルファベット＋0～9までの数字の合計62文字の中からランダムで10桁の文字を求めるプログラム。
+
+<!-- 
 (詳しい説明はこちらにflum.sun.ddns.vc/twitter/php_benchmark/index.html)
 
 推奨環境はlinux環境でphpが必要です。
 動作チェックはUbuntu13.04のPHP5.5.9でしました。
+--> 
 
-
-##How to Use
+## How to Use
 
 ```
-$ git clone https://github.com/flum1025/php_benchmark.git
+$ git clone https://github.com/sogaoh/php_benchmark.git
 $ cd php_benchmark
-$ php php_benchmark.rb
+$ php php_benchmark.php
 
 ```
 
 
-質問等ありましたらTwitter:[@flum_](https://twitter.com/flum_)までお願いします。
+質問等ありましたら Twitter:[@sogaoh](https://twitter.com/sogaoh) までご連絡ください。
 
+<!--
 ##License
 
 The MIT License
 
 -------
 (c) @2015 flum_
+-->
+
+[^1]: 「php ベンチマーク」で探して見つかった https://github.com/flum1025/php_benchmark を少し改変したものです。
--- php_benchmark.php
+++ php_benchmark.php
@@ -1,12 +1,16 @@
-﻿<?php
-mb_language("ja");
-mb_internal_encoding("UTF-8");
+<?php
+#mb_language("ja");
+#mb_internal_encoding("UTF-8");
 error_reporting(E_ALL & ~E_NOTICE);
 
-$times = 100000000;
-echo "${times}回ループします\n";
+#$times = 100000000;
+$times  = 1000000;
+#$times = 10;
+#echo "${times}回ループします\n";
+echo "loop ${times} times\n";
 
 $str = array_merge ( range ( 'a', 'z' ), range ( '0', '9' ), range ( 'A', 'Z' ) );
+//var_dump($str);
 
 $starttime = microtime(TRUE);
 $startmsec = explode('.', $starttime);
@@ -16,7 +20,10 @@ $count = 0;
 while ( $count < $times ) {
 	$r_str = null;
 	for($i = 0; $i < 10; $i ++) {
-		$r_str .= $str [rand ( 0, count ( $str ) )];
+		//$r_str .= $str [rand ( 0, count ( $str ) )];
+		$rand = rand ( 0, count ( $str ) ) ;
+		$index = $rand >= 1 ? $rand - 1 : 0;
+		$r_str .= $str [$index];
 	}
 	$count ++;
 }
@@ -28,4 +35,3 @@ echo date("i:s",$endtime) . "." . $endmsec[1] . "\n";
 $margin = $endtime - $starttime;
 $marginmsec = explode('.', $margin);
 echo date("i:s",$margin) . "." . $marginmsec[1] . "\n";
-?>
